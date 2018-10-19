4bbfc43dbfd52e2924b4cb027087b665928bfb35
CSU <CSU@CSU-TOSH>
2018-10-19 09:16:52 +0200

    Zadatak 2
    Zbir brojeva izmedju dva uneta broja.

--- src/Demo.java
+++ src/Demo.java
@@ -1,14 +1,33 @@
+import java.util.Scanner;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int sum = 0;
-		for(int i = 0; i < 11; i++) {
-			sum = sum + i; 
-			}
-		 System.out.println("Suma brojev je " + sum);
+		System.out.println("Unesite prvi broj: ");
+		Scanner br1 = new Scanner(System.in);
+		int broj1 = br1.nextInt();
+		
+		System.out.println("Unesite drugi broj: ");
+		Scanner br2 = new Scanner(System.in);
+		int broj2 = br2.nextInt(); 
+		
+		
+		if (broj2 < broj1) {
+			System.out.println("Ukoliko je prvi broj veci od drugog, brojevi ce biti zamenjeni.");
+			int broj = broj2;
+			broj2 = broj1;
+			broj1 = broj;
+		}
+		
+		int zbir = 0;
+		
+		for (int i = broj1; i <= broj2; i++) {
+			zbir = zbir + i;
+		}
+		
+		System.out.println("Zbir brojeva je " + zbir + ".");
 		
 	}
 
