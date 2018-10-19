d9045398c268283b459971841ce56d653a43958f
CSU <CSU@CSU-TOSH>
2018-10-19 08:50:56 +0200

    Zadatak 1: Suma brojeva od 1 do 10.

---
+++ src/Demo.java
@@ -0,0 +1,15 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum = 0;
+		for(int i = 0; i < 11; i++) {
+			sum = sum + i; 
+			}
+		 System.out.println("Suma brojev je " + sum);
+		
+	}
+
+}
